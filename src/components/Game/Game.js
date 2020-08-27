53edcf7a0f3462458bd8976d11f0197fa2faf1f7
Adam <aiskrzycki01@gmail.com>
2020-08-27 13:44:10 +0200

    Started building the board

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import classes from './Game.module.css';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import Tile from '../Game/Tile/Tile';
 
 class Game extends Component {
     render() {
-        return <div>HELLO FROM THE OTHER SIDE</div>
+        return (
+            <>
+                <nav className={classes.NavBar}><Button variant='contained' component={Link} to='/'>Home</Button></nav>
+                <main className={classes.GameContainer}>
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                </main>
+            </>
+        )
     }
 }
 
---
+++ src/components/Game/Tile/Tile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import classes from './Tile.module.css';
+
+class Tile extends Component {
+    render() {
+        return <div className={classes.Tile}></div>
+    }
+}
+
+export default Tile;
+
