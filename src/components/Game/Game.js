e12bd118803ae26e94e9106fd2a321fc49d557e3
Adam <aiskrzycki01@gmail.com>
2020-08-28 11:35:31 +0200

    Added some buttons and basic board structure

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,32 +1,26 @@
 import React, { Component } from 'react';
 import classes from './Game.module.css';
-import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import Tile from '../Game/Tile/Tile';
 
 class Game extends Component {
     render() {
+
+        const easyMode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
         return (
             <>
-                <nav className={classes.NavBar}><Button variant='contained' component={Link} to='/'>Home</Button></nav>
+                <nav className={classes.NavBar}>
+                    <Link to='/'>
+                        <button className={classes.HomeButton}>Home</button>
+                    </Link>
+                </nav>
                 <main className={classes.GameContainer}>
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
-                    <Tile />
+                    {easyMode.map(el => {
+                        return <Tile />
+                    })}
                 </main>
+                <button className={classes.PlayButton}>Play</button>
             </>
         )
     }
