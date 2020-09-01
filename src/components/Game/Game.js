8bcfd632195b92d94b95e98cfe989712deb8647e
Adam <aiskrzycki01@gmail.com>
2020-09-01 15:10:31 +0200

    Added an onclick handling color changes

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,10 +3,16 @@ import classes from './Game.module.css';
 import { Link } from 'react-router-dom';
 import Tile from '../Game/Tile/Tile';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import { IconButton } from '@material-ui/core';
+import { IconButton, Hidden } from '@material-ui/core';
 
 class Game extends Component {
+
+    state = {
+        hasGameStarted: false            
+    }
+
     render() {
+
 
         const iconStyles = {
             fontSize: '50px',
@@ -17,6 +23,10 @@ class Game extends Component {
         const easyMode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
         const easyModeTiles = [];
 
+        const gameStartHandler = () => {
+            this.setState({hasGameStarted: true})
+        }
+
         return (
             <>
                 <nav className={classes.NavBar}>
@@ -26,11 +36,12 @@ class Game extends Component {
                 </nav>
                 <main className={classes.GameContainer}>
                     {easyMode.map((index) => {
-                        easyModeTiles.push(<Tile key={index} tiles={easyModeTiles}/>)
+                        easyModeTiles.push(<Tile key={index} random={this.state.hasGameStarted ? Math.floor(Math.random() * 2) : 0} />)
                     })}
+                    {console.log(Math.floor(Math.random() * easyModeTiles.length))}
                     {easyModeTiles}
                 </main>
-                <button className={classes.PlayButton}>Play</button>
+                <button className={classes.PlayButton} onClick={gameStartHandler} >Play</button>
             </>
         )
     }
--- src/components/Game/Tile/Tile.js
+++ src/components/Game/Tile/Tile.js
@@ -2,10 +2,8 @@ import React from 'react';
 import classes from './Tile.module.css';
 
 const Tile = (props) => {
-
-    const random = Math.floor(Math.random() * props.tiles.length);
     
-    return <div className={random > 4 ? classes.Tile : classes.RandomTile}></div>
+    return <div className={props.random === 0 ? classes.Tile : classes.RandomTile}></div>
 }
 
 export default Tile;
