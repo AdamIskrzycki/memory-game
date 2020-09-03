30d6c4dd75d728b408f5886c7a3618cf50a8898d
Adam <aiskrzycki01@gmail.com>
2020-09-03 13:14:03 +0200

    Improved game mechanism

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,12 +3,13 @@ import classes from './Game.module.css';
 import { Link } from 'react-router-dom';
 import Tile from '../Game/Tile/Tile';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import { IconButton, Hidden } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
+import _ from 'lodash';
 
 class Game extends Component {
 
     state = {
-        hasGameStarted: false            
+        hasGameStarted: false
     }
 
     render() {
@@ -20,25 +21,26 @@ class Game extends Component {
             color: 'white',
         }
 
-        const easyMode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        const easyMode = _.range(16);
         const easyModeTiles = [];
 
         const gameStartHandler = () => {
-            this.setState({hasGameStarted: true})
+            setTimeout(() => { this.setState({ hasGameStarted: true }) }, 250)
+            setTimeout(() => { this.setState({ hasGameStarted: false }) }, 750)
         }
 
         return (
             <>
                 <nav className={classes.NavBar}>
                     <IconButton component={Link} to='/'>
-                        <ArrowBackIcon style={iconStyles}/>
+                        <ArrowBackIcon style={iconStyles} />
                     </IconButton>
                 </nav>
                 <main className={classes.GameContainer}>
                     {easyMode.map((index) => {
                         easyModeTiles.push(<Tile key={index} random={this.state.hasGameStarted ? Math.floor(Math.random() * 2) : 0} />)
                     })}
-                    {console.log(Math.floor(Math.random() * easyModeTiles.length))}
+                    {/* {console.log(Math.floor(Math.random() * easyModeTiles.length))} */}
                     {easyModeTiles}
                 </main>
                 <button className={classes.PlayButton} onClick={gameStartHandler} >Play</button>
