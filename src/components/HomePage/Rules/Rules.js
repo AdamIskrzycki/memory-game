c9b34640f24b184a0fe52e8e955f4b7b46415e88
Adam <aiskrzycki01@gmail.com>
2021-09-06 20:24:37 +0200

    Updated README and content of Rules component

--- src/components/HomePage/Rules/Rules.js
+++ src/components/HomePage/Rules/Rules.js
@@ -8,7 +8,7 @@ const Rules = () => {
                 <header className={classes.Header}>RULES</header>
                 <section className={classes.Rules}>The rules are pretty simple.
                      The board consists of boxes. As soon as you hit 'play' some of them will change their color.
-                      After that, your job is to mark which of the boxes changed their color.<br /> Good luck! </section>
+                      After that, your job is to mark the boxes which changed their color.<br /> Good luck! </section>
             </article>
         </>
     )
